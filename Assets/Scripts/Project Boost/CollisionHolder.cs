50afc1a84a05b15f9918b6a20571f24db1814dc0
Coebe <youareaaron0@gmail.com>
2022-08-28 19:00:19 +0800

    project update change object to prefab, add particle

---
+++ Assets/Scripts/Project Boost/CollisionHolder.cs
@@ -0,0 +1,118 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public delegate void SequenceDelegate();
+
+public class CollisionHolder : MonoBehaviour
+{
+    static public CollisionHolder instance;
+    [SerializeField] float loadDelay = 1f;
+    [SerializeField] AudioClip crash;
+    [SerializeField] AudioClip finish;
+    [SerializeField] ParticleSystem crashParticle;
+    [SerializeField] ParticleSystem finishParticle;
+
+    AudioSource audioSource;
+
+    bool isTransationing = false;
+    bool collisionEnabled = true;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        instance = this;
+    }
+
+    private void Update()
+    {
+        RespondDebugKeys();
+    }
+
+    void RespondDebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            CollisionHolder.instance.NextLevel();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            collisionEnabled = !collisionEnabled;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    static void DoSequence(bool tag, SequenceDelegate sequenceDelegate)
+    {
+        if (tag) sequenceDelegate();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (isTransationing || !collisionEnabled) return;
+        audioSource.Stop();
+        // 将方法绑定(+=)到委托, 等到委托触发时会一次调用绑定的方法
+        // 将方法解除绑定(-=)
+        // SequenceDelegate delegate1 = new SequenceDelegate(StartCrashSequence);
+        // delegate1 += StartSuccessSequence;
+        DoSequence(other.gameObject.CompareTag("Respawn"), StartCrashSequence);
+        DoSequence(other.gameObject.CompareTag("Finish"), StartSuccessSequence);
+        // 使用委托(Delegate)替代 switch
+        // switch (other.gameObject.tag)
+        // {
+        //     case "Friendly":
+        //         Debug.Log("you have touched the friendly tag object~~");
+        //         break;
+        //     case "Fuel":
+        //         Debug.Log("come on!");
+        //         break;
+        //     case "Respawn":
+        //         StartCrashSequence();
+        //         break;
+        //     case "Finish":
+        //         StartSuccessSequence();
+        //         break;
+        //     default:
+        //         Debug.Log("here is~~ other thing");
+        //         break;
+        // }
+    }
+
+    void StartSuccessSequence()
+    {
+        Debug.Log("StartSuccessSequence");
+        isTransationing = true;
+        audioSource.PlayOneShot(finish);
+        // TODO: add partical upon crash BUT build and run no problem
+        finishParticle.Play();
+        GetComponent<Movement>().enabled = false;
+        Invoke("NextLevel", loadDelay);
+    }
+
+    void StartCrashSequence()
+    {
+        Debug.Log("StartCrashSequence");
+        isTransationing = true;
+        audioSource.PlayOneShot(crash);
+        // TODO: add partical upon crash
+        crashParticle.Play();
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", loadDelay);
+    }
+    public void ReloadLevel()
+    {
+        // SceneManager.LoadScene(levelName);
+        // this way of statement is more easily to read
+        int currentActiveLevel = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentActiveLevel);
+    }
+
+    public void NextLevel()
+    {
+        int nextLevel = SceneManager.GetActiveScene().buildIndex + 1;
+        nextLevel = nextLevel < SceneManager.sceneCountInBuildSettings ? nextLevel : 0;
+        SceneManager.LoadScene(nextLevel);
+    }
+}
--- Assets/Scripts/Project Boost/Movement.cs
+++ Assets/Scripts/Project Boost/Movement.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField] float rotateValue = 1f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] ParticleSystem particleLeft;
+    [SerializeField] ParticleSystem particleMain;
+    [SerializeField] ParticleSystem particleRight;
     public float thrustSpeed = 1f;
 
     Rigidbody rg;
+    AudioSource audioSource;
+    BoxCollider collision;
 
     // Start is called before the first frame update
     void Start()
     {
         rg = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        collision = GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
@@ -26,7 +34,11 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rg.AddRelativeForce(Vector3.up * Time.deltaTime * thrustSpeed);
+            StartThrusting();
+        }
+        else if (audioSource.isPlaying)
+        {
+            StopThrusting();
         }
     }
 
@@ -35,21 +47,65 @@ public class Movement : MonoBehaviour
         Vector3 direction = Vector3.zero;
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            direction = Vector3.forward;
-            ApplyRotate(direction);
+            StartRotateForward(ref direction);
         }
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            direction = Vector3.back;
-            ApplyRotate(direction);
+            StartRotateBack(ref direction);
+        }
+        else
+        {
+            StopRotate();
         }
 
     }
 
-    void ApplyRotate(Vector3 direction)
+    void StartThrusting()
+    {
+        // TODO: 火箭尾部粒子特效经常没有
+        particleMain.Play();
+        rg.AddRelativeForce(Vector3.up * Time.deltaTime * thrustSpeed);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+    }
+
+    void StopThrusting()
+    {
+        audioSource.Stop();
+        particleMain.Stop();
+    }
+
+    void StartRotateForward(ref Vector3 direction)
+    {
+        direction = Vector3.forward;
+        ApplyRotate(ref direction);
+        particleRight.Play();
+    }
+
+    void StartRotateBack(ref Vector3 direction)
+    {
+        direction = Vector3.back;
+        ApplyRotate(ref direction);
+        particleLeft.Play();
+    }
+
+    void StopRotate()
+    {
+        particleRight.Stop();
+        particleLeft.Stop();
+    }
+
+    void ApplyRotate(ref Vector3 direction)
     {
         rg.freezeRotation = true;   // freeze rotation when key pressed 'A' or 'left arrow'
         transform.Rotate(direction * rotateValue * Time.deltaTime);
         rg.freezeRotation = false;  // unfressze rotation when key release 
+    }
+
+    public void Respawn()
+    {
+
     }
 }
